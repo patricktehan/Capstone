965cf0a62f2c4c600867a79c27795f4417fec689
JackMcDonald1 <jack.mcdonald.19@cnu.edu>
2022-10-17 20:18:41 -0400

    got both sides working properly and pulling information from database

--- Gambling_Gateway/mock.php
+++ Gambling_Gateway/mock.php
@@ -48,7 +48,7 @@ $rush_yard_offense_2 = '';
 
 //$sql = mysqli_select_db('2021NFL_stats',$conn);
 $query = $conn->query("SELECT * FROM 2021NFL_stats");
-
+$query2 = $conn->query("SELECT * FROM 2021NFL_stats");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -65,65 +65,98 @@ $query = $conn->query("SELECT * FROM 2021NFL_stats");
         <div class="pick1">
             <table >
                 <th>
-                    <form method="POST" action="">
+                    <form method="GET" action="">
                     <select name="team1">
+                        <option value="Choose">Choose a team!</option>
                         <?php
                         while($rows = $query->fetch_assoc()){
                             $team_name = $rows['Team_name'];
                             echo "<option value = '$team_name'>$team_name</option>";
                         }
                         ?>
-
                         <input type="submit" name="submit" value="submit">
                     </select>
                     </form>
                     <br>
 
                     <?php
-                    $selection_team1 = $_POST['team1'];
+                    $selection_team1 = $_GET['team1'];
                     echo $selection_team1;
-
-                    $team_query = $conn->query("SELECT * FROM 2021NFL_stats where Team_name = '$selection_team1'");
-                    while ($row = mysqli_fetch_array($team_query)){
-                        $division = $row['Division'];
-                        $division_score = $row['Division_score'];
-                        $head_coach_ranking = $row['Head_coach_rating'];
-                        $offense_ranting = $row['offense_rating'];
-                        $defense_ranting = $row['defense_rating'];
-                        $QB_ranting = $row['QB_rating'];
-                        $record_ats_away = $row['record_ATS_away'];
-                        $record_ats_away_fav = $row['record_ATS_away_fav'];
-                        $ats_home = $row['ats_home'];
-                        $ats_home_underdog = $row['ats_home_underdog'];
-                        $ats_away_underdog = $row['ats_away_underdog'];
-                        $ats_home_fav = $row['ats_home_fav'];
-                        $third_down_offense = $row['3rd_down_offense'];
-                        $third_down_defense = $row['3rd_down_defense'];
-                        $pass_yard_offense = $row['pass_yard_offense'];
-                        $pass_yard_defense = $row['pass_yard_defense'];
-                        $rush_yard_defense = $row['rush_yard_defense'];
-                        $rush_yard_offense = $row['rush_yard_offense'];
-                    }
                     ?>
 
                 </th>
             </table>
             <br>
         </div>
-
         <div class="pick2">
             <table>
                 <th>
-                    <select name="team2">
-                        <?php
-                        while($rows = $query->fetch_assoc()){
-                            $team_name_2 = $rows['Team_name'];
-                            echo "<option value = '$team_name'>$team_name</option>";
-                        }
-                        ?>
-                    </select>
+                    <form method="POST" action="">
+                        <select name="team2">
+                            <option value="Choose">Choose a team!</option>
+                            <?php
+                            while($rows = $query2->fetch_assoc()){
+                                $team_name_2 = $rows['Team_name'];
+                                echo "<option value = '$team_name_2'>$team_name_2</option>";
+                            }
+                            ?>
+                            <input type="submit" name="submit2" value="submit">
+                        </select>
+                    </form>
+                    <br>
+                    <?php
+                    $selection_team2 = $_POST['team2'];
+                    echo $selection_team2;
+                    ?>
                 </th>
             </table>
+
+            <?php
+
+            $team_query = $conn->query("SELECT * FROM 2021NFL_stats where Team_name = '$selection_team1'");
+            while ($row = mysqli_fetch_array($team_query)){
+                $division = $row['Division'];
+                $division_score = $row['Division_score'];
+                $head_coach_ranking = $row['Head_coach_rating'];
+                $offense_ranting = $row['offense_rating'];
+                $defense_ranting = $row['defense_rating'];
+                $QB_ranting = $row['QB_rating'];
+                $record_ats_away = $row['record_ATS_away'];
+                $record_ats_away_fav = $row['record_ATS_away_fav'];
+                $ats_home = $row['ats_home'];
+                $ats_home_underdog = $row['ats_home_underdog'];
+                $ats_away_underdog = $row['ats_away_underdog'];
+                $ats_home_fav = $row['ats_home_fav'];
+                $third_down_offense = $row['3rd_down_offense'];
+                $third_down_defense = $row['3rd_down_defense'];
+                $pass_yard_offense = $row['pass_yard_offense'];
+                $pass_yard_defense = $row['pass_yard_defense'];
+                $rush_yard_defense = $row['rush_yard_defense'];
+                $rush_yard_offense = $row['rush_yard_offense'];
+            }
+            $team_query_2 = $conn->query("SELECT * FROM 2021NFL_stats where Team_name = '$selection_team2'");
+            while ($row = mysqli_fetch_array($team_query_2)){
+                $division_2 = $row['Division'];
+                $division_score_2 = $row['Division_score'];
+                $head_coach_ranking_2 = $row['Head_coach_rating'];
+                $offense_ranting_2 = $row['offense_rating'];
+                $defense_ranting_2 = $row['defense_rating'];
+                $QB_ranting_2 = $row['QB_rating'];
+                $record_ats_away_2 = $row['record_ATS_away'];
+                $record_ats_away_fav_2 = $row['record_ATS_away_fav'];
+                $ats_home_2 = $row['ats_home'];
+                $ats_home_underdog_2 = $row['ats_home_underdog'];
+                $ats_away_underdog_2 = $row['ats_away_underdog'];
+                $ats_home_fav_2 = $row['ats_home_fav'];
+                $third_down_offense_2 = $row['3rd_down_offense'];
+                $third_down_defense_2 = $row['3rd_down_defense'];
+                $pass_yard_offense_2 = $row['pass_yard_offense'];
+                $pass_yard_defense_2 = $row['pass_yard_defense'];
+                $rush_yard_defense_2 = $row['rush_yard_defense'];
+                $rush_yard_offense_2 = $row['rush_yard_offense'];
+            }
+            ?>
+
         </div>
         <div class="bet">
             <table>
@@ -223,7 +256,7 @@ $query = $conn->query("SELECT * FROM 2021NFL_stats");
         <div class="team2">
             <table>
                 <tr>
-                    <td> <?php echo $division_2 ?> <p> The cardinals give up 150 rushing yards per game.</p></td>
+                    <td> <?php echo $division_2 ?></td>
                 </tr>
                 <tr>
                     <td> <?php echo $division_score_2 ?> </td>
